b1b3dd375727518a5d18b2f436384c7c93dbb40d
KAREN <lafuentekaren@gmail.com>
2019-06-20 16:01:32 -0300

    letter, map, const, digit and more added

--- scala/src/main/scala/Parser.scala
+++ scala/src/main/scala/Parser.scala
@@ -1,20 +1,45 @@
-trait Parser[A] {
+import org.scalactic.ErrorMessage
+
+trait Parser[A] { self =>
   def apply(input: String):Result[A]
+
+  def const[B] (b:B): Parser[B] = map(_ => b)
+
+  def map[B](function: A =>B) : Parser[B] = new Parser[B]{
+    override def apply(input: String): Result[B] = self(input) match {
+      case success : Success[A]=> success.copy(value = function(success.value))
+      case failure : Failure => failure
+    }
+  }
 }
 
 object anyChar extends Parser[Char] {
-  override def apply(input: String): Result[Char] = input match {
-    case "" => Failure("empty string")
-    case _ => Success(input.head, input.tail)
-  }
+  override def apply(input: String): Result[Char] = charSatisfies(_ => true)(input)
 }
 
 object char {
-  def apply(expected: Char) : Parser[Char] = new Parser[Char] {
+  def apply(expected: Char) : Parser[Char] = charSatisfies(_ == expected, actual => s"$actual is not $expected")
+}
+
+object charSatisfies {
+  def apply(condition : Char => Boolean, errorMessage: Char => String = x => s"$x did not match") : Parser[Char] = new Parser[Char] {
     override def apply(input: String): Result[Char] = input match {
       case "" => Failure("empty string")
-      case x if x.head == expected => Success(input.head, input.tail)
-      case _ => Failure(s"${input.head} is not $expected")
+      case x if condition(x.head) => Success(input.head, input.tail)
+      case _ => Failure(errorMessage(input.head))
     }
   }
+}
+
+//Preguntar lo que tiene que devolver esta función: Unit type?
+object void extends Parser[Unit]{
+  override def apply(input: String): Result[Unit] = anyChar.const()(input)
+}
+
+object letter extends Parser[Char]{
+  override def apply(input: String): Result[Char] = charSatisfies(_.isLetter, actual => s"$actual is not letter")(input)
+}
+
+object digit extends Parser[Char]{
+  override def apply(input: String): Result[Char] = charSatisfies(_.isDigit, actual => s"$actual is not digit")(input)
 }
--- scala/src/test/scala/ProjectSpec.scala
+++ scala/src/test/scala/ProjectSpec.scala
@@ -31,4 +31,48 @@ class ProjectSpec extends FreeSpec with Matchers {
     val Failure(reason) = char('x')("")
     reason shouldBe "empty string"
   }
+
+  "void should parse hola" in {
+    val Success(parsed,remaining) = void("hola")
+    parsed shouldBe ()
+    remaining shouldBe "ola"
+  }
+
+  "const replaces whatever this parser parses" in {
+    val Success(parsed, _) = anyChar.const("Karen es la mas kpa")("hola")
+    parsed shouldBe "Karen es la mas kpa"
+  }
+
+  "parser.const fails when parser fails" in {
+    val parser = char('x')
+    val input = "karen"
+    parser.const()(input) shouldBe parser(input)
+  }
+
+  "parser.map transform to upper" in {
+    val parser = anyChar.map(_.toUpper)
+    val input = "batman"
+    val Success(parsed, _) = parser(input)
+    parsed shouldBe 'B'
+  }
+
+  "digit should parse when is digit" in {
+    val Success(parsed, _) = digit("1234444")
+    parsed shouldBe '1'
+  }
+
+  "digit should not parse when is non digit" in {
+    val Failure(reason) = digit("k1234444")
+    reason shouldBe "k is not digit"
+  }
+
+  "letter should parse when is a letter" in {
+    val Success(parsed, _) = letter("karen")
+    parsed shouldBe 'k'
+  }
+
+  "letter should not parse when is non letter" in {
+    val Failure(reason) = letter("0la")
+    reason shouldBe "0 is not letter"
+  }
 }
