f23d1f2d75c2cdf434b85c18a3ad3105862b245f
KAREN <lafuentekaren@gmail.com>
2019-06-20 14:27:48 -0300

    char added

--- scala/src/main/scala/Parser.scala
+++ scala/src/main/scala/Parser.scala
@@ -4,7 +4,17 @@ trait Parser[A] {
 
 object anyChar extends Parser[Char] {
   override def apply(input: String): Result[Char] = input match {
-  case "" => Failure("empty string")
-  case _ => Success(input.head, input.tail)
+    case "" => Failure("empty string")
+    case _ => Success(input.head, input.tail)
+  }
 }
+
+object char {
+  def apply(expected: Char) : Parser[Char] = new Parser[Char] {
+    override def apply(input: String): Result[Char] = input match {
+      case "" => Failure("empty string")
+      case x if x.head == expected => Success(input.head, input.tail)
+      case _ => Failure(s"${input.head} is not $expected")
+    }
+  }
 }
--- scala/src/test/scala/ProjectSpec.scala
+++ scala/src/test/scala/ProjectSpec.scala
@@ -8,7 +8,6 @@ class ProjectSpec extends FreeSpec with Matchers {
     remaining shouldBe "ola"
   }
 
-
   "anyChar should parse chau" in {
     val Success(char, remaining) = anyChar("chau")
     char shouldBe 'c'
@@ -16,6 +15,20 @@ class ProjectSpec extends FreeSpec with Matchers {
   }
   "anyChar should not parse empty string" in {
     val Failure(reason) = anyChar("")
+    reason shouldBe "empty string"
+  }
+
+  "char('x') should parse string started with x" in {
+    val Success(first, remaining) = char('x')("xol")
+    first shouldBe 'x'
+    remaining shouldBe "ol"
+  }
+  "char('z') should not parse string started with x" in {
+    val Failure(reason) = char('x')("zol")
+    reason shouldBe "z is not x"
+  }
+  "char('z') should not parse empty string" in {
+    val Failure(reason) = char('x')("")
     reason shouldBe "empty string"
   }
 }
