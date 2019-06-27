aed57634e4f8dc67174be3cba15ee0380dd6a606
rodr0m4 <rodrigo.martin@mercadolibre.com>
2019-06-26 23:25:00 -0300

    Removed non-describing names

--- scala/src/main/scala/parser/Prueba.scala
+++ scala/src/main/scala/parser/Prueba.scala
@@ -2,15 +2,14 @@ package parser
 
 case object Prueba extends App {
 
-  def asd: Parser[Number] = (digit.+ <> (char('.') <> digit.+).?).map {
+  def coolSyntax: Parser[Number] = (digit.+ <> (char('.') <> digit.+).?).map {
     case integralPart ~ None => integralPart.mkString.toInt
     case integralPart ~ Some(dot ~ fractionalPart) =>
       (integralPart.toString + dot.toString + fractionalPart.toString).toDouble
   }
 
-  def jjj = string("val") <> letter.+ <> char('=') <> digit.+ map {
-    // This chain does not work in tests :(q
-    case _ ~ identifier ~ _ ~ expr => (identifier, expr)
+  def doesNotWorkInTests: Parser[Double] = string("val") <> letter.+ <> char('=') <> digit.+ map {
+    case _ ~ identifier ~ _ ~ expr => (identifier ++ "." ++ expr).mkString.toInt
   }
 
   def materia: String = "parser"
