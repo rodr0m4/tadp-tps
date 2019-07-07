ef6d7346101cc92eb19649bab3e78c7ae6f61966
KAREN <lafuentekaren@gmail.com>
2019-07-07 01:44:01 -0300

    tests

---
+++ scala/src/test/scala/MusiquitaSpec.scala
@@ -0,0 +1,52 @@
+import musiquita.{Blanca, Musiquita, Negra, Nota, Redonda, Silencio, SinAccidente, F}
+import org.scalatest.{FreeSpec, Matchers}
+import parser.char.ExpectedButFound
+import parser.string.DoesNotStartWithException
+
+import scala.util.{Failure, Success}
+
+class MusiquitaSpec extends FreeSpec with Matchers{
+  val idOfA = 9
+  "silencio should parse a silence" in {
+    val Success((parsed, remaining)) = Musiquita.silencio("-A")
+    val Silencio(nota) = parsed
+    nota.duracion shouldBe Redonda.duracion/4
+    remaining shouldBe "A"
+  }
+  "silencio should not parse a figure" in {
+    val Failure(reason) = Musiquita.silencio("4-A")
+    reason shouldBe ExpectedButFound('~', '4')
+  }
+  "nota should parse singleton note" in {
+    val Success((parsed, remaining)) = Musiquita.nota("A-A")
+    Nota.notaToId(parsed) shouldBe idOfA
+    remaining shouldBe "-A"
+  }
+  "nota should parse composed note" in {
+    val Success((parsed, remaining)) = Musiquita.nota("A#-A")
+    Nota.notaToId(parsed) shouldBe idOfA + 1
+    remaining shouldBe "-A"
+  }
+  "nota should not parse a silence" in {
+    val Failure(reason) = Musiquita.nota("-A")
+    reason shouldBe ExpectedButFound('B','-')
+  }
+  "figura should parse a figure" in {
+    val Success((parsed, remaining)) = Musiquita.figura("1/2A")
+    parsed shouldBe Blanca
+    remaining shouldBe "A"
+  }
+  "figura should not parse something that is not a figure" in {
+    val Failure(reason) = Musiquita.figura("1//2")
+    reason shouldBe DoesNotStartWithException("16","/2")
+  }
+  "tono should parse a tone" in {
+    val Success((parsed, remaining)) = Musiquita.tono("6A#1/4")
+    parsed.octava shouldBe 6
+    parsed.nota shouldBe F(SinAccidente)
+    remaining shouldBe "1/4"
+  }
+  "tono should not parse a figure" in {
+
+  }
+}
